e84021f8e5da0a5a628f63b8137b8f4938806f3f
Soham Tolwala <srtolwala11@gmail.com>
2025-06-13 11:49:49 +0200

    new models added

--- submission_simclr.py
+++ submission_simclr.py
@@ -6,10 +6,12 @@ import os
 # === CONFIG ===
 SEED = "61437433"      
 TOKEN = "09596680"    
-# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR.onnx"        # alpha = 0.5
-# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_07.onnx"          # alpha = 0.7
-ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_09.onnx"          # alpha = 0.9
-# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_cosine.onnx"          # alpha = 0.7 _&_ cosine loss
+# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR.onnx"                     # MSE_alpha = 0.5          L2 = 6.81
+# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_07.onnx"                  # MSE_alpha = 0.7          L2 = 6.6186
+# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_09.onnx"                  # MSE_alpha = 0.9          L2 = 6.6148
+ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_cosine_norm_loss.onnx"   # cosine_norm_alpha = 0.9  L2 = 19.93
+# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_cosine.onnx"              # alpha = 0.7 _&_ cosine loss   L2 = ?
+# ONNX_PATH = "F:\\TML_model_stealing\\TML25_A2_39\\models\\model_Sim_CLR_NT_xent.onnx"             # NT xent _&_ tau = 1.0        L2 = ?
 
 # === Validate ONNX Model ===
 print("\U0001F50D Testing ONNX format...")
